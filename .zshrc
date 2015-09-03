026cbd246603f8eb3936829b6283382a2b9bbe5b
nipe <nipe0324@gmail.com>
2015-09-04 01:36:41 +0900

    zprezo

--- .zshrc
+++
@@ -1,1 +0,0 @@
-/Users/nipe/.zprezto/runcoms/zshrc
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -10,8 +10,13 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
 # zsh
 # TODO: peco
-ln -sf ~/dotfiles/.zshrc    ~/.zshrc
-ln -sf ~/dotfiles/.zprezto  ~/.zprezto
+mkdir zsh_orig && mv zshmv .zlogin .zlogout .zprofile .zshenv .zshrc .zprezto zsh_orig 
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+	  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+	done
+done
 
 # tmux
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
